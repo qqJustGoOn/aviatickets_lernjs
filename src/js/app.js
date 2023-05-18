6828036f69e02504ac5db3084597d81792a6a2d7
qqJustGoOn <66961636+qqJustGoOn@users.noreply.github.com>
2023-05-18 14:38:11 +0300

    added Store locations

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,12 @@
-import api from './services/apiService';
+//import api from './services/apiService';
 
-api.countries().then(res => console.log(res));
-api.cities().then(res => console.log(res));
+// api.countries().then(res => console.log(res));
+// api.cities().then(res => console.log(res));
+
+import locations from './store/locations';
+
+locations.init().then(res => {
+  console.log(res);
+  console.log(locations);
+  console.log(locations.getCitiesByCountryCode('PE'));
+});
---
+++ src/js/store/locations.js
@@ -0,0 +1,32 @@
+import api from '../services/apiService';
+
+
+class Locations {
+  constructor(api) {
+    this.api = api;
+    this.contries = null;
+    this.cities = null;
+  }
+
+  async init() {
+    const response = await Promise.all([
+      this.api.countries(),
+      this.api.cities()
+    ]);
+
+    const [countries, cities] = response;
+    this.countries = countries;
+    this.cities = cities;
+
+    return response;
+  }
+
+  //метод получения городов по коду страны
+  getCitiesByCountryCode(code) {
+    return this.cities.filter(city => city.country_code === code);
+  }
+}
+
+const locations = new Locations(api);
+
+export default locations;
