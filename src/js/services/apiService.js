29f3f221235dbcb6bd05a33d7dca3d40eb86612e
qqJustGoOn <66961636+qqJustGoOn@users.noreply.github.com>
2023-05-18 13:25:34 +0300

    Знакомство с api service

--- src/js/services/apiService.js
+++ src/js/services/apiService.js
@@ -1,12 +1,35 @@
 import axios from 'axios';
 import config from '../config/apiConfig';
 
+//countries - array of countries
+//cities - array of cities
+//prices/cheap - array
+
 //Класс с набором методов для взаимодействия с нашим сервером
 class Api {
   constructor(config) {
     this.url = config.url;
   }
-  
+//методы запросов на сервер
+  async countries() {
+    try {
+      const response = await axios.get(`${this.url}/countries`);
+      return response.data;
+    } catch(err) {
+      console.log(err);
+      return Promise.reject(err);
+    }
+  }
+  async cities() {
+    try {
+      const response = await axios.get(`${this.url}/cities`);
+      return response.data;
+    } catch(err) {
+      console.log(err);
+      return Promise.reject(err);
+    }
+  }
+  async prices(params) {}
 }
 
 
