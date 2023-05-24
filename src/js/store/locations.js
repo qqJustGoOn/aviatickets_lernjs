56a4055ee2bdb05b21c417cfbdc878aa0f6a044c
qqJustGoOn <66961636+qqJustGoOn@users.noreply.github.com>
2023-05-24 10:52:57 +0300

    data serialization

--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -15,10 +15,38 @@ class Locations {
     ]);
 
     const [countries, cities] = response;
-    this.countries = countries;
-    this.cities = cities;
+    this.countries = this.serializeCountries(countries);
+    this.cities = this.serializeCities(cities);
 
     return response;
+  }
+
+
+
+  serializeCountries(countriesData) {
+    //Приводим массив стран к такому виду:
+    // { 'Country code': {*тело страны*} }  "Country code" - ключ
+    return countriesData.reduce((acc, country) => {
+      //в новый объект закидываем под ключом кода страны ее объект!
+      acc[country.code] = country;
+      return acc;
+    }, {});
+  }
+
+  serializeCities(citiesData) {
+    // { 'City name, Country name': {*тело города*} }
+    return citiesData.reduce((acc, city) => {
+      const country_name = this.getCountryNameByCode(city.country_code);
+      const city_name = city.name || city.name_translations.en; //если нет русского названия города, возьмет англ. интерпретацию
+      const key = `${city_name}, ${country_name}`;
+      acc[key] = city;
+      return acc;
+    }, {});
+  }
+
+  getCountryNameByCode(countryCode) {
+   // { 'Country code': {*тело страны*} }
+    return this.countries[countryCode].name;
   }
 
   //метод получения городов по коду страны
@@ -30,3 +58,6 @@ class Locations {
 const locations = new Locations(api);
 
 export default locations;
+
+
+// { 'City, Country': null } - вид объекта для автокомплита
